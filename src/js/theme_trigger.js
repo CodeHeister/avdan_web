cb1ed7a3789ffb1d93b19204aaff5a367db249a5
CodeHeister <extremal.user@gmail.com>
2022-07-06 00:22:45 +0300

    Header done

--- src/js/theme_trigger.js
+++ src/js/theme_trigger.js
@@ -3,7 +3,7 @@ let root = document.documentElement;
 var is_light = false;
 
 toggle_theme.addEventListener('click', e => {
-  if (!is_light) {
+  if (!is_light) { // white
     root.style.setProperty('--cursor-color', '48,48,48');
     root.style.setProperty('--font-primary', '#000');
     root.style.setProperty('--body-bg', 'white');
@@ -11,8 +11,13 @@ toggle_theme.addEventListener('click', e => {
     root.style.setProperty('--scrollbar-thumb-color', '#5E5E5E');
     root.style.setProperty('--scrollbar-track-bg', '#EEEEEE');
     root.style.setProperty('--icon-fill', 'black');
+    root.style.setProperty('--map-bg', '225,225,225');
+    root.style.setProperty('--toggle-theme-color', '#0dcaf0');
+    root.style.setProperty('--menu-download-color', 'white');
+
+	document.querySelector(".toggle-theme").innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>';
 	is_light = true;
-  } else {
+  } else { // black
     root.style.setProperty('--cursor-color', '225,225,225');
     root.style.setProperty('--font-primary', '#fff');
     root.style.setProperty('--body-bg', 'black');
@@ -20,6 +25,11 @@ toggle_theme.addEventListener('click', e => {
     root.style.setProperty('--scrollbar-thumb-color', '#aaa');
     root.style.setProperty('--scrollbar-track-bg', '#101010');
     root.style.setProperty('--icon-fill', 'white');
+    root.style.setProperty('--map-bg', '48,48,48');
+    root.style.setProperty('--toggle-theme-color', '#ffc107');
+    root.style.setProperty('--menu-download-color', 'black');
+
+	document.querySelector(".toggle-theme").innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>';
 	is_light = false;
   }
 })
