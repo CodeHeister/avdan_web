5b9bc3546606df40402be4c471375c2a58a4bc71
CodeHeister <extremal.user@gmail.com>
2022-07-06 13:24:45 +0300

    Added and tested header location underline, cursor size fixed, disabled some colors

--- src/js/location_check.js
+++ src/js/location_check.js
@@ -1,1 +1,9 @@
-var current_location = window.location.href;
+var current_location = window.location.href.split("?")[0].split("#")[0].split("/").pop();
+
+var locs = [["index.html", "avdanos.com", "avdan_web"], ["features-beta", "features-beta.html"], ["downloads"], ["support"], ["docs"], ["demo"]];
+
+for (var i = 0; i < locs.length; i++) {
+	if (locs[i].includes(current_location)) {
+		document.querySelectorAll(".map .map-item")[i].classList.add("current");
+	}
+}
--- src/js/theme_trigger.js
+++ src/js/theme_trigger.js
@@ -7,13 +7,13 @@ toggle_theme.addEventListener('click', e => {
     root.style.setProperty('--cursor-color', '232,233,235');
     root.style.setProperty('--font-primary', '#000');
     root.style.setProperty('--font-secondary', '#5E5E5E');
-    root.style.setProperty('--body-bg', 'white');
     root.style.setProperty('--scrollbar-thumb-bg', '#9e9e9e');
     root.style.setProperty('--scrollbar-track-bg', '#5e5e5e');
     root.style.setProperty('--icon-fill', 'black');
     root.style.setProperty('--map-bg', '225,225,225');
     root.style.setProperty('--toggle-theme-color', '#0dcaf0');
     root.style.setProperty('--menu-bg', 'rgba(225,225,225,0.8)');
+    root.style.setProperty('--link-color', '#234BE8');
 
 	document.querySelector(".toggle-theme").innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>';
 	is_light = true;
@@ -21,13 +21,13 @@ toggle_theme.addEventListener('click', e => {
     root.style.setProperty('--cursor-color', '225,225,225');
     root.style.setProperty('--font-primary', '#fff');
     root.style.setProperty('--font-secondary', '#a0a0a0');
-    root.style.setProperty('--body-bg', 'black');
     root.style.setProperty('--scrollbar-thumb-bg', '#333');
     root.style.setProperty('--scrollbar-track-bg', '#101010');
     root.style.setProperty('--icon-fill', 'white');
     root.style.setProperty('--map-bg', '48,48,48');
     root.style.setProperty('--toggle-theme-color', '#ffc107');
     root.style.setProperty('--menu-bg', 'rgba(48,48,48,0)');
+    root.style.setProperty('--link-color', '#3291FF');
 
 	document.querySelector(".toggle-theme").innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>';
 	is_light = false;
