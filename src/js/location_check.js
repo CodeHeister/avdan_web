f9534cbea422e11d216c9152b06699656e193873
CodeHeister <extremal.user@gmail.com>
2022-07-06 13:34:16 +0300

    Fixed location check

--- src/js/location_check.js
+++ src/js/location_check.js
@@ -1,4 +1,9 @@
-var current_location = window.location.href.split("?")[0].split("#")[0].split("/").pop();
+var current_locs = window.location.href.split("?")[0].split("#")[0].split("/");
+
+var current_location = current_locs.pop()
+while (current_location == "") {
+	current_location = current_locs.pop()
+}
 
 var locs = [["index.html", "avdanos.com", "avdan_web"], ["features-beta", "features-beta.html"], ["downloads"], ["support"], ["docs"], ["demo"]];
 
