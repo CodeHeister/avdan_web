fc72b4b2e1baba4124da470b7108cf6a42035f2f
CodeHeister <extremal.user@gmail.com>
2022-07-06 15:26:12 +0300

    Changed panel scroll design

--- src/js/panel_control.js
+++ src/js/panel_control.js
@@ -2,13 +2,21 @@
 const panelModify = e => {
 	if (window.scrollY > 0) {
 		document.querySelectorAll(".toggle-lang, .toggle-theme, .menu-download").forEach(item => {item.style.display = "none";});
-		document.querySelector(".icon").style.marginRight = "initial";
+		document.querySelector(".icon").style.display = "none";
 		document.querySelector(".menu").style.backgroundColor = "var(--menu-bg-scrolled)";
+		document.querySelector(".menu").style.padding = "0";
+		document.querySelector(".menu").style.height = "3.5rem";
+		document.querySelector(".map").style.background = "none";
+		document.querySelector(".map").style.margin = "initial";
 	}
 	else {
 		document.querySelectorAll(".toggle-lang, .toggle-theme, .menu-download").forEach(item => {item.style.display = null;});
-		document.querySelector(".icon").style.marginRight = null;
+		document.querySelector(".icon").style.display = null;
 		document.querySelector(".menu").style.backgroundColor = null;
+		document.querySelector(".menu").style.padding = null;
+		document.querySelector(".menu").style.height = null;
+		document.querySelector(".map").style.background = null;
+		document.querySelector(".map").style.margin = null;
 	}
 }
 
