18dac419184cce6ff8d72790788cef0c4993eff8
CodeHeister <extremal.user@gmail.com>
2022-07-11 22:55:40 +0300

    Resize test

--- src/js/test.js
+++ src/js/test.js
@@ -1,36 +1,32 @@
 var z_index_g = 1;
 
-const resize = (e, target) => {
-	target.style.top = 0;
-	target.style.left = 0;
-	target.style.width = "100%";
-	target.style.height = "100%";
-	target.style.transform = null;
+const resize = (e, target, info) => {
+	target.style.width = target.offsetWidth/2+"px";
+	target.style.height = target.offsetHeight/2+"px";
 	target.style.zIndex = z_index_g;
 	z_index_g += 1;
 }
 
-const resizeBack = (e, target) => {
-	target.style.background = "black";
-	target.style.height = "500px";
-	target.style.width = "512px";
+const resizeBack = (e, target, info) => {
+	target.style.width = target.offsetWidth*2+"px";
+	target.style.height = target.offsetHeight*2+"px";
 }
 
-const recolorp = (e, target) => {
+const recolorp = (e, target, info) => {
 }
 
-const recolorBackp = (e, target) => {
+const recolorBackp = (e, target, info) => {
 }
 
-const recolor = (e, target) => {
+const recolor = (e, target, info) => {
 	target.style.zIndex = z_index_g;
 	z_index_g += 1;
 }
 
-const recolorBack = (e, target) => {
+const recolorBack = (e, target, info) => {
 }
 
-const attach = (e, target) => {
+const attach = (e, target, info) => {
 	if (e.screenX > window.innerWidth/2) {
 		target.style.top = 0;
 		target.style.left = "50%";
@@ -51,40 +47,63 @@ const attach = (e, target) => {
 	}
 }
 
-const dragResizeWL = (e, target) => {
+const dragResizeWL = (e, target, info) => {
 	target.style.width = `${target.clientWidth-e.movementX}px`;
 }
 
-const dragResizeWR = (e, target) => {
+const dragResizeWR = (e, target, info) => {
 	target.style.width = `${target.clientWidth+e.movementX}px`;
 }
 
-const dragResizeHT = (e, target) => {
+const dragResizeHT = (e, target, info) => {
 	target.style.height = `${target.clientHeight-e.movementY}px`;
 }
 
-const dragResizeHB = (e, target) => {
+const dragResizeHB = (e, target, info) => {
 	target.style.height = `${target.clientHeight+e.movementY}px`;
 }
 
-const dragResizeWHLT = (e, target) => {
+const dragResizeWHLT = (e, target, info) => {
 	target.style.width = `${target.clientWidth-e.movementX}px`;
 	target.style.height = `${target.clientHeight-e.movementY}px`;
 }
 
-const dragResizeWHRT = (e, target) => {
+const dragResizeWHRT = (e, target, info) => {
 	target.style.width = `${target.clientWidth+e.movementX}px`;
 	target.style.height = `${target.clientHeight-e.movementY}px`;
 }
 
-const dragResizeWHLB = (e, target) => {
+const dragResizeWHLB = (e, target, info) => {
 	target.style.width = `${target.clientWidth-e.movementX}px`;
 	target.style.height = `${target.clientHeight+e.movementY}px`;
 }
 
-const dragResizeWHRB = (e, target) => {
+const dragResizeWHRB = (e, target, info) => {
 	target.style.width = `${target.clientWidth+e.movementX}px`;
 	target.style.height = `${target.clientHeight+e.movementY}px`;
+}
+
+const winCheck = (e, target, info) => {
+	document.querySelectorAll(".window").forEach(item => {
+		if (document.querySelector(info.target) != item) {
+			var targetTransformX = 0;
+			var targetTransformY = 0;
+			if (item.style.transform != '') {
+			
+				var nums = item.style.transform.split("translate3d")[1];
+				nums = nums.slice(1, nums.length-1).split("px,");
+				
+				targetTransformX = parseInt(nums[0]);
+				targetTransformY = parseInt(nums[1]);
+			}
+
+			var x1 = item.offsetLeft+targetTransformX;
+			var x2 = x1+item.offsetWidth;
+			var y1 = item.offsetTop+targetTransformY;
+			var y2 = y1+item.offsetHeight;
+			console.log(x1, x2, y1, y2, x1 <= e.clientX && e.clientX <= x2 && y1 <= e.clientY && e.clientY <= y2);
+		}
+	});
 }
 
 var str1 = ".resize1";
@@ -94,10 +113,10 @@ var test2 = document.querySelector(str2);
 
 window.addEventListener("mousemove", drag);
 
-test1.addEventListener("click", e => {click(e, ".window1", undefined, resize)});
 document.body.addEventListener("mouseup", e => {leave(e, ".window1")});
 
-document.querySelector(".panel1").addEventListener("mousedown", e => {dragAdd(e, ".panel1", ".window1", recolorp, recolor, undefined, undefined, recolorBackp, resizeBack, attach)});
+test1.addEventListener("mousedown", e => {dragAdd(e, str1, ".window1", undefined, resize, undefined, undefined, winCheck, resizeBack, undefined, true, true, document.querySelector(".window1").offsetWidth/2)});
+document.querySelector(".panel1").addEventListener("mousedown", e => {dragAdd(e, ".panel1", ".window1", undefined, undefined, undefined, undefined, undefined, undefined, attach)});
 document.querySelector(".wl1").addEventListener("mousedown", e => {dragAdd(e, ".wl1", ".window1", undefined, undefined, undefined, dragResizeWL, undefined, undefined, undefined, true, false)});
 document.querySelector(".wr1").addEventListener("mousedown", e => {dragAdd(e, ".wr1", ".window1", undefined, undefined, undefined, dragResizeWR, undefined, undefined, undefined, false, false)});
 document.querySelector(".ht1").addEventListener("mousedown", e => {dragAdd(e, ".ht1", ".window1", undefined, undefined, undefined, dragResizeHT, undefined, undefined, undefined, false, true)});
@@ -109,7 +128,7 @@ document.querySelector(".whrb1").addEventListener("mousedown", e => {dragAdd(e, 
 
 document.body.addEventListener("mouseup", e => {leave(e, ".window1")});
 
-test2.addEventListener("mousedown", e => {dragAdd(e, str2, ".window2", recolorp, recolor, undefined, undefined, recolorBackp, resizeBack, attach)});
+test2.addEventListener("mousedown", e => {dragAdd(e, str2, ".window2", undefined, undefined, undefined, undefined, undefined, undefined, attach)});
 document.body.addEventListener("mouseup", e => {leave(e, ".window2")});
 document.querySelector("html").addEventListener("mouseleave", leaveAll);
 /*
