974995c436ea75d2a18a629e24a17becd8b74478
CodeHeister <extremal.user@gmail.com>
2022-07-13 23:55:42 +0300

    JS files renamed

--- src/js/windows.js
+++ src/js/windows.js
@@ -701,38 +701,3 @@ const appBarGenerate = apps_list_l => {
 
 window.addEventListener("mousemove", drag);
 document.querySelector("html").addEventListener("mouseleave", leaveAll);
-
-// M A I N
-
-var content1 = document.createElement("div");
-content1.innerHTML = "Hi";
-
-var content2 = document.createElement("div");
-content2.innerHTML = "Hello";
-
-var content3 = document.createElement("div");
-content3.innerHTML = "Test3";
-
-content1.classList.add("noselect");
-content2.classList.add("noselect");
-content3.classList.add("noselect");
-
-var apps_list_g = [
-	{
-		"title" : "Calculator",
-		"src" : "src/images/demo/icons/Apps/Calculator.png",
-		"content" : content1
-	},
-	{
-		"title" : "Calendar",
-		"src" : "src/images/demo/icons/Apps/Calendar.png",
-		"content" : content2
-	},
-	{
-		"title" : "AfterEffects",
-		"src" : "src/images/demo/icons/Apps/AfterEffects.png",
-		"content" : content3
-	},
-]
-
-appBarGenerate(apps_list_g);
