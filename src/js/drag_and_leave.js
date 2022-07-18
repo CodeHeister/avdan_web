1c974136ecfc8baf4829c9a9de196fab9e355473
CodeHeister <extremal.user@gmail.com>
2022-07-18 20:40:39 +0300

    Comments added

--- src/js/drag_and_leave.js
+++ src/js/drag_and_leave.js
@@ -1,11 +1,15 @@
+// main drag list (wipe to drop all)
 var dragList = [];
 
-// mousedown event (control element)
+// mousedown event (add to dragList)
 const dragAdd = (e, currentTarget, target, currentTarget_in_f, target_in_f, currentTarget_f, target_f, currentTarget_out_f, target_out_f, drop_f, modify_X, modify_Y, extraX, extraY) => {
-	if (e.target == e.currentTarget) {
+	if (e.target == e.currentTarget) { // check if target
 
+		// check if disable X or Y moving
 		if (modify_X == undefined) modify_X = true
 		if (modify_Y == undefined) modify_Y = true
+
+		// generate info
 		var info = {
 			"currentTarget" : currentTarget,
 			"target" : target,
@@ -21,21 +25,28 @@ const dragAdd = (e, currentTarget, target, currentTarget_in_f, target_in_f, curr
 			"extraX" : extraX || 0,
 			"extraY" : extraY || 0
 		};
+
+		// append info
 		dragList.push(info);
 
+		// get target
 		var target = document.querySelector(target);
 
+		// control element function on drag start
 		if (typeof currentTarget_in_f == "function") {
 			currentTarget_in_f(e, e.currentTarget, info);
 		}
 
+		// target function on drag start
 		if (typeof target_in_f == "function") {
 			target_in_f(e, target, info);
 		}
 
+		// default value
 		var targetTransformX = 0;
 		var targetTransformY = 0;
 
+		// get target transform
 		if (target.style.transform != '') {
 		
 			var nums = target.style.transform.split("translate3d")[1];
@@ -45,10 +56,12 @@ const dragAdd = (e, currentTarget, target, currentTarget_in_f, target_in_f, curr
 			targetTransformY = parseInt(nums[1]);
 		}
 
+		// ad extraX or extraY
 		target.style.transform = `translate3d(${targetTransformX+(extraX || 0)}px,${targetTransformY+(extraY || 0)}px,0)`;
 	}
 }
 
+// extra click function control -> target
 const click = (e, target, currentTarget_f, target_f) => {
 	
 	var target = document.querySelector(target);
@@ -62,8 +75,10 @@ const click = (e, target, currentTarget_f, target_f) => {
 	}
 }
 
-//mousemove event (window)
+// mousemove event (window)
 const drag = e => {
+
+	// go through dragList
 	dragList.forEach(item => {
 		var target = document.querySelector(item.target);
 		var targetTransformX = 0;
@@ -77,10 +92,12 @@ const drag = e => {
 			targetTransformY = parseInt(nums[1]);
 		}
 
+		// control element function while drag
 		if (typeof item.currentTarget_f == "function") {
 			item.currentTarget_f(e, document.querySelector(item.currentTarget), item);
 		}
 
+		// target function while drag
 		if (typeof item.target_f == "function") {
 			item.target_f(e, target, item);
 		}
@@ -94,8 +111,10 @@ const drag = e => {
 //mouseup event (control element) (also may be mouseout as main drop) 
 const leave = (e, target) => { // event, move target, fuction for control, function for target
 	
+	// default value for info
 	var info = '';
 
+	// get target from dragList
 	for (var i = 0; i < dragList.length; i++) { 
     
         if ( dragList[i].target == target) { 
@@ -105,14 +124,19 @@ const leave = (e, target) => { // event, move target, fuction for control, funct
     
     }
 	
+	// if not default
 	if (info != '') {
+
+		// get targets
 		var target = document.querySelector(target);
 		var currentTarget = document.querySelector(info.currentTarget);
 
+		// control element on drop
 		if (typeof info.currentTarget_out_f == "function") {
 			info.currentTarget_out_f(e, currentTarget, info);
 		}
 
+		// target function on drop
 		if (typeof info.target_out_f == "function") {
 			info.target_out_f(e, target, info);
 		}
@@ -126,25 +150,31 @@ const leave = (e, target) => { // event, move target, fuction for control, funct
 			var targetTransformY = parseInt(nums[1]);
 		}
 
+		// delete extraX and extraY
 		target.style.transform = `translate3d(${targetTransformX-info.extraX}px,${targetTransformY-info.extraY}px,0)`;
 	}
 }
 
 
 const leaveAll = e => {
+
+	// get all items from dragList
 	dragList.forEach(info => {
 		
 		var target = document.querySelector(info.target);
 		var currentTarget = document.querySelector(info.currentTarget);
 
+		// control element function on drop
 		if (typeof info.currentTarget_out_f == "function") {
 			info.currentTarget_out_f(e, e.currentTarget, info);
 		}
 
+		// target function on drop
 		if (typeof info.target_out_f == "fuction") {
 				info.target_out_f(e, target, info);
 		}
 
+		// target function on out_of_bounds
 		if (typeof info.drop_f != "function") {
 			info.drop_f(e, target, info);
 		}
@@ -159,9 +189,10 @@ const leaveAll = e => {
 			var targetTransformY = parseInt(nums[1]);
 		}
 
+		// delete extraX and extraY
 		target.style.transform = `translate3d(${targetTransformX-info.extraX}px,${targetTransformY-info.extraY}px,0)`;
 	});
 
-
+	// wipe list 
 	dragList = [];
 }
