33cdfe4da0ae0740d61018ecba5fe2df89acd826
CodeHeister <extremal.user@gmail.com>
2022-07-20 13:38:25 +0300

    Added menu bar placeholder, added scroll bar player

--- src/js/demo.js
+++ src/js/demo.js
@@ -489,6 +489,7 @@ var apps_list_g = [
 appBarGenerate(apps_list_g);
 // -- //
 
+
 // -- W E A T H E R  &  T I M E
 
 var weather_time = document.createElement("div");
@@ -520,7 +521,9 @@ time_panel.classList.add("time-panel");
 time_panel.appendChild(curr_time);
 time_panel.appendChild(curr_date);
 
-var weather_icon = document.createElement("img");
+var weather_icon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+weather_icon.setAttributeNS(null, "viewBox", "0 0 24 24");
+weather_icon.innerHTML = '<circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>';
 
 var weather_grad = document.createElement("div");
 weather_grad.classList.add("weather-grad");
@@ -536,12 +539,65 @@ weather_time.appendChild(weather_panel);
 
 // -- //
 
+var player = document.createElement("div");
+player.classList.add("player");
+player.classList.add("noselect");
+
+var player_icon = document.createElement("div");
+player_icon.classList.add("player-icon");
+
+var player_icon_img = document.createElement("img");
+player_icon_img.src = "src/images/player_img.jpg";
+player_icon.appendChild(player_icon_img);
+
+var player_info = document.createElement("div");
+player_info.classList.add("player-info");
+
+var player_title = document.createElement("div");
+player_title.classList.add("player-title");
+player_title.innerHTML = "Une belle historie";
+
+var plyer_artist = document.createElement("div");
+plyer_artist.classList.add("player-artist");
+plyer_artist.innerHTML = "Michel Fugain";
+
+player_info.appendChild(player_title);
+player_info.appendChild(plyer_artist);
+
+var player_control = document.createElement("div");
+player_control.classList.add("player-control");
+
+var player_prev = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+player_prev.setAttributeNS(null, "viewBox", "0 0 24 24");
+player_prev.innerHTML = '<path d="M0 21 v -15 c 0 0 -0.5 -4 3 -3 l 12 7.5 c 0 0 1.5 1.5 0 3 l -12 7.5 c 0 0 -4 1 -3 -3 z m 12 -16 c 0 0 -2 0 -1 2.268 l 5.888 3.732 c 0 0 1 1 0 2 l -3.888 2.732 c 0 0 -4.5 2.5 -1 3.268 l 11 -6 c 0 0 1 -1 0 -2 l -11 -6 z"></path>';
+player_prev.style.transform = "rotate(180deg)";
+player_prev.style.fill = "#ffffff";
+
+var player_pause = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+player_pause.setAttributeNS(null, "viewBox", "0 0 24 24");
+player_pause.innerHTML = '<path d="M10 24 h -3 c 0 0 -3 0 -3 -3 v -18 c 0 0 0 -3 3 -3 c 0 0 3 0 3 3 v 18 c 0 0 0 3 -3 3 z m 10 0 h -3 c 0 0 -3 0 -3 -3 v -18 c 0 0 0 -3 3 -3 c 0 0 3 0 3 3 v 18 c 0 0 0 3 -3 3 z m -11 -23 h -2 c 0 0 -2 0 -2 2 v 18 c 0 0 0 2 2 2 c 0 0 2 0 2 -2 v -18 c 0 0 0 -2 -2 -2 z m 10 0 h -2 c 0 0 -2 0 -2 2 v 18 c 0 0 0 2 2 2 c 0 0 2 0 2 -2 v -18 c 0 0 0 -2 -2 -2 z"></path>';
+player_pause.style.fill = "#ffffff";
+
+var player_next = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+player_next.setAttributeNS(null, "viewBox", "0 0 24 24");
+player_next.innerHTML = '<path d="M0 21 v -15 c 0 0 -0.5 -4 3 -3 l 12 7.5 c 0 0 1.5 1.5 0 3 l -12 7.5 c 0 0 -4 1 -3 -3 z m 12 -16 c 0 0 -2 0 -1 2.268 l 5.888 3.732 c 0 0 1 1 0 2 l -3.888 2.732 c 0 0 -4.5 2.5 -1 3.268 l 11 -6 c 0 0 1 -1 0 -2 l -11 -6 z"></path>';
+player_next.style.fill = "#ffffff";
+
+player_control.appendChild(player_prev);
+player_control.appendChild(player_pause);
+player_control.appendChild(player_next);
+
+player.appendChild(player_icon);
+player.appendChild(player_info);
+player.appendChild(player_control);
+
 // -- S C R O L L  B A R  C O N F I G S
 
 var scroll_list_g = { 
 	"pos" : 0,
 	"items" : [
 		weather_time,
+		player,
 	]
 }
 
